10baf4f5a71cc50c3aa8e6482dd59ae3943d5025
Monu Nigaam <monunigaam1658@gamil.com>
2025-11-23 19:20:38 +0530

    fix: downgrade express to version 4.21.2 for compatibility; enhance server.js to serve static files in production

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import path from 'path';
+
 
 // local imports
 import authRouter from './Routes/authRouter.js';
@@ -14,10 +16,13 @@ import connectDB from './config/mongodb.js';
 
 dotenv.config(); 
 
+
 const app = express();
 
 const originList = ['http://localhost:5173'];
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -26,15 +31,30 @@ app.use(cors({
   credentials: true, // Allow cookies to be sent
 }))
 
-app.get("/",(req,res)=>{
-  res.send("API is running...");
-})
+
 
 
 app.use('/api/auth',authRouter);
 app.use('/api/users',userRouter);
 app.use('/api/chat',chatRouter);
 
+console.log("__dirname =", __dirname);
+
+
+if (process.env.NODE_ENV === "production") {
+
+  const distPath = path.join(__dirname, "../client/dist");
+
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}else{
+  app.get("/",(req,res)=>{
+  res.send("API is running...");
+})
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on : http://localhost:${PORT}`);
